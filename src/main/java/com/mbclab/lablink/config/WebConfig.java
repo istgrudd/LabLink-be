0a1937b33cd710193b4a317cb621e70045fbfdcb
istgrudd <rudifirdaus233@gmail.com>
2026-01-16 22:45:31 +0700

    delete CORS web

--- src/main/java/com/mbclab/lablink/config/WebConfig.java
+++ src/main/java/com/mbclab/lablink/config/WebConfig.java
@@ -3,7 +3,6 @@ package com.mbclab.lablink.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -28,18 +27,5 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper().findAndRegisterModules();
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Izinkan semua endpoint
-                .allowedOrigins(
-                    "http://localhost:3000", // Biar di laptop tetap jalan
-                    "https://lablink-fe.vercel.app", // Ganti dengan domain Vercel kamu nanti
-                    "*" // ATAU pakai bintang dulu buat testing sementara (Hati-hati, ini membuka ke semua orang)
-                )
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
     }
 }
